a2fcf450451269a5606c93937b66d7e2e2732bff
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-05-09 13:28:11 +0900

    #1.0 Starting the Driver

--- reservation.py
+++ reservation.py
@@ -1,9 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-url = "https://songdotennis.co.kr/reservation"
-
-response = requests.get(url)
-
-print(response)
-print(response.text)
+# Set chrome driver
+chrome_options = webdriver.ChromeOptions()
+browser = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=chrome_options
+)
+browser.get("https://google.com")

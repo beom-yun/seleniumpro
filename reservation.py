221efa69d65e2c5810706c9e5f9fcfe6fb70c271
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-05-09 19:35:09 +0900

    #1.2 Finding Results

--- reservation.py
+++ reservation.py
@@ -17,4 +17,7 @@ search_bar.send_keys("hello!")
 search_bar.send_keys(Keys.ENTER)
 
 search_results = browser.find_elements(By.CLASS_NAME, "g")
-print(search_results)
+for search_result in search_results:
+    title = search_result.find_element(By.TAG_NAME, "h3")
+    if title:
+        print(title.text)

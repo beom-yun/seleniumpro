f02b45e7ab3b91f44e6a5c22e1c7b800d4d22c6c
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-05-09 19:24:36 +0900

    #1.1 Searching

--- reservation.py
+++ reservation.py
@@ -1,10 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Set chrome driver
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 browser = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()), options=chrome_options
 )
+
 browser.get("https://google.com")
+search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
+search_bar.send_keys("hello!")
+search_bar.send_keys(Keys.ENTER)
+
+search_results = browser.find_elements(By.CLASS_NAME, "g")
+print(search_results)
